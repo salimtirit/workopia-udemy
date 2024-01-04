0a0f559138d70fbd8b0ac9a412aac832b1f186b4
Salim <salimtirit58@gmail.com>
2024-01-04 16:55:26 +0300

    database connection creating file

---
+++ Database.php
@@ -0,0 +1,20 @@
+<?php
+class Database
+{
+    public $conn;
+
+    public function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        try {
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int) $e->getCode());
+        }
+    }
+}
