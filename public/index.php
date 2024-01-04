7331f415c7ebf77fc671a5d3dab34840b7d78bc6
Salim <salimtirit58@gmail.com>
2024-01-04 16:55:37 +0300

    using the database

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
 <?php
 require '../helpers.php';
+require basePath('Database.php');
+
+$config = require basePath('config/db.php');
+
+$db = new Database($config);
+
 require basePath('Router.php');
 
 $router = new Router();
 
-$routes = require basePath('routes.php'); 
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
