2a53760fba801048efb5f6da47cdc17fa6cb9c1d
Salim <salimtirit58@gmail.com>
2024-01-04 15:57:47 +0300

    router and routes work way changed

---
+++ Router.php
@@ -0,0 +1,54 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+
+    public function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    public function error($httpCode = 404)
+    {
+        http_response_code($httpCode);
+        loadView('error/' . $httpCode);
+        exit;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
+}
