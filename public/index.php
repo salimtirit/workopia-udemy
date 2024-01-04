31140d1692d096770cfdc00ce5b9ff6a2cfe3306
Salim <salimtirit58@gmail.com>
2024-01-04 14:31:13 +0300

    routing mechanism added. Will be moved to another file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 require '../helpers.php';
-require basePath('views/home.view.php');
 
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
 
+$uri = $_SERVER['REQUEST_URI'];
+
+if(array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+}
+else {
+    require(basePath($routes['404']));
+}
