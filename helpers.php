7a6965ae91fcbcaafbe69d77ce4613e9db5f289c
Salim <salimtirit58@gmail.com>
2024-01-04 14:13:48 +0300

    created load view and load partial functions

--- helpers.php
+++ helpers.php
@@ -6,6 +6,41 @@
  * @param string $path
  * @return string
  */
- function basePath ($path = '') {
-     return __DIR__ . '/' . $path;
- }
+function basePath($path = '')
+{
+    return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (!file_exists($viewPath)) {
+        echo "View {$name} not found";
+        return;
+    }
+    require $viewPath;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (!file_exists($partialPath)) {
+        echo "Partial {$name} not found";
+        return;
+    }
+    require $partialPath;
+}
