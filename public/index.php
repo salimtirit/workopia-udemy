47b0e5aadf6621af87989703820478c7d0910f53
Salim <salimtirit58@gmail.com>
2024-01-04 17:14:06 +0300

    router added to the index

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
 <?php
 require '../helpers.php';
-require basePath('Database.php');
-
-$config = require basePath('config/db.php');
-
-$db = new Database($config);
-
 require basePath('Router.php');
+require basePath('Database.php');
 
 $router = new Router();
 
