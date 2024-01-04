eb02f55233550f154e0f83427ab668e226aaca62
Salim <salimtirit58@gmail.com>
2024-01-04 17:14:25 +0300

    query function added

--- Database.php
+++ Database.php
@@ -9,10 +9,22 @@ class Database
 
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
         try {
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int) $e->getCode());
+        }
+    }
+
+    public function query($query)
+    {
+        try {
+            $sth = $this->conn->prepare($query);
+            $sth->execute();
+            return $sth;
         } catch (PDOException $e) {
             throw new PDOException($e->getMessage(), (int) $e->getCode());
         }
