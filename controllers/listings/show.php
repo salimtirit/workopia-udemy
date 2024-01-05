3476a2bdc73fbd0bcc61b393533d649792d328f2
Salim <salimtirit58@gmail.com>
2024-01-05 11:58:37 +0300

    showing one listing with actual data handled

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -1,3 +1,15 @@
 <?php
 
-loadView('listings/show');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$id = $_GET['id'] ?? ''; // TODO: there may be no id
+
+$params = [
+    'id' => $id
+];
+$listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+loadView('listings/show', [
+    'listing' => $listing
+]);
