e98cdb6fb7b71b6e676a9c7c0d906c988dc8c95d
Salim <salimtirit58@gmail.com>
2024-01-09 10:39:11 +0300

    destroy method added for the DELETE method

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -118,4 +118,24 @@ class ListingController
             redirect('/listings');
         }
     }
+
+    function destroy($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+
+        redirect('/listings');
+    }
 }
