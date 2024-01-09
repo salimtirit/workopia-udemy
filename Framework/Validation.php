82d5017a8c7f3cb366b66eeb483e7e70864cb0a4
Salim <salimtirit58@gmail.com>
2024-01-09 14:13:02 +0300

    validare number (for salary mainly) and validate phone  number added

--- Framework/Validation.php
+++ Framework/Validation.php
@@ -22,4 +22,29 @@ class Validation
     {
         return trim($value) === trim($match);
     }
+
+    public static function validateNumber($value, $min = 0, $max = INF)
+    {
+        if (!is_numeric($value)) {
+            return false;
+        }
+        return $value >= $min && $value <= $max;
+    }
+
+    public static function validatePhoneNumber($value)
+    {
+        $value = trim($value);
+        $value = str_replace(' ', '', $value);
+        if (str_starts_with($value, '+')) {
+            $value = substr($value, 1);
+        }
+        if (str_contains($value, '-')) {
+            $value = str_replace('-', '', $value);
+        }
+        if (!is_numeric($value)) {
+            return false;
+        }
+        //TODO: better regex can be written for universal phone number validation
+        return true;
+    }
 }
