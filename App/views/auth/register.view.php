fe738cf5cc61d1af207b7744a7e67219aa141057
Salim <salimtirit58@gmail.com>
2024-01-09 16:55:31 +0300

    shows error if there is an error and fields are filled if error heppens

--- App/views/auth/register.view.php
+++ App/views/auth/register.view.php
@@ -3,18 +3,25 @@
 <div class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
         <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
-        <form>
+        <?php if (isset($errors)) : ?>
+            <div class="message bg-red-100 p-3 my-3">
+                <?php foreach ($errors as $error) : ?>
+                    <div><?= $error ?></div>
+                <?php endforeach ?>
+            </div>
+        <?php endif ?>
+        <form method="POST" action="/auth/register">
             <div class="mb-4">
-                <input type="text" name="name" placeholder="Full Name" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="name" placeholder="Full Name" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $user['name'] ?? '' ?>" />
             </div>
             <div class="mb-4">
-                <input type="email" name="email" placeholder="Email Address" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="email" name="email" placeholder="Email Address" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $user['email'] ?? '' ?>" />
             </div>
             <div class="mb-4">
-                <input type="text" name="city" placeholder="City" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="city" placeholder="City" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $user['city'] ?? '' ?>" />
             </div>
             <div class="mb-4">
-                <input type="text" name="state" placeholder="State" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="state" placeholder="State" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $user['state'] ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input type="password" name="password" placeholder="Password" class="w-full px-4 py-2 border rounded focus:outline-none" />
