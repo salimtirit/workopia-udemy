658784f6ba33dc9726b031834a96e6f8e664b120
Salim <salimtirit58@gmail.com>
2024-01-09 16:53:58 +0300

    store method now validates the written fields and shows an error if there is a problem with them. does not save to db yet

--- App/controllers/AuthController.php
+++ App/controllers/AuthController.php
@@ -29,6 +29,38 @@ class AuthController
 
     public function store()
     {
+        $errors = [];
+        if (!Validation::validateEmail($_POST['email'])) {
+            $errors['email'] = 'Email is invalid';
+        }
+
+        $passwordValidationMessage = Validation::validatePassword($_POST['password']);
+        if ($passwordValidationMessage !== true) {
+            $errors['password'] = $passwordValidationMessage;
+        }
+
+        if (!Validation::validateMatch($_POST['password'], $_POST['password_confirmation'])) {
+            $errors['password_confirmation'] = 'Passwords do not match';
+        }
+
+        foreach ($_POST as $key => $value) {
+            if (empty($value) || !Validation::validateString($_POST[$key])) {
+                if ($key === 'city' || $key === 'state') {
+                    continue;
+                }
+                $errors[$key] = ucfirst(str_replace('_', ' ', $key)) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('auth/register', [
+                'errors' => $errors,
+                'user' => $_POST
+            ]);
+            return;
+        }
+
+        // TODO: check if email already exists
     }
 
     public function authenticate()
