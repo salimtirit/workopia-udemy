b77c80086d8df63f0d8ac75e3ecce51c76eb0276
Salim <salimtirit58@gmail.com>
2024-01-09 16:20:18 +0300

    get and post methods of register and login added

--- routes.php
+++ routes.php
@@ -8,3 +8,7 @@ $router->get('/listings/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
+$router->get('/auth/register', 'AuthController@register');
+$router->post('/auth/register', 'AuthController@store');
+$router->get('/auth/login', 'AuthController@login');
+$router->post('/auth/login', 'AuthController@authenticate');
