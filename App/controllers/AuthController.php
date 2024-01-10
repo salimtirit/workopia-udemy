31141ecf5d403a28e617785679116e5319425ae7
Salim <salimtirit58@gmail.com>
2024-01-10 16:50:04 +0300

    sessin for register and login functions added

--- App/controllers/AuthController.php
+++ App/controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 
 class AuthController
@@ -85,10 +86,103 @@ class AuthController
         $query = 'INSERT INTO users (name, city, state, email, password) VALUES (:name, :city, :state, :email, :password)';
         $this->db->query($query, $params);
 
+        // Get the new user id
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' =>   $userId,
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'city' => $_POST['city'],
+            'state' => $_POST['state']
+        ]);
+
         redirect('/');
     }
 
     public function authenticate()
     {
+        $errors = [];
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if (!Validation::validateString($email)) {
+            $errors['email'] = 'Email is required';
+        }
+
+        if (!empty($errors)) {
+            loadView('auth/login', [
+                'errors' => $errors,
+                'user' => $_POST
+            ]);
+            return;
+        }
+
+        if (!Validation::validateEmail($email)) {
+            $errors['email'] = 'Email is invalid';
+        }
+
+        if (!Validation::validateString($password)) {
+            $errors['password'] = 'Password is required';
+        }
+
+        if (!empty($errors)) {
+            loadView('auth/login', [
+                'errors' => $errors,
+                'user' => $_POST
+            ]);
+            return;
+        }
+
+        $params = [
+            'email' => $email
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+        if (!$user) {
+            $errors['email'] = 'Email does not exist';
+            loadView('auth/login', [
+                'errors' => $errors,
+                'user' => $_POST
+            ]);
+            return;
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            $errors['password'] = 'Password is incorrect';
+            loadView('auth/login', [
+                'errors' => $errors,
+                'user' => $_POST
+            ]);
+            return;
+        }
+
+        Session::set('user', [
+            'id' =>   $user['id'],
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'city' => $user['city'],
+            'state' => $user['state']
+        ]);
+
+        redirect('/');
+    }
+
+    public function logout()
+    {
+        Session::destroy();
+
+        $params = session_get_cookie_params();
+
+        setcookie(
+            'PHPSESSID',
+            '',
+            time() - 86400,
+            $params['path'],
+            $params['domain']
+        );
+
+        redirect('/');
     }
 }
