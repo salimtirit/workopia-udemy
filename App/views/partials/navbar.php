8351c1b1f5f14319054fd35f3567d7f4ef2d1828
Salim <salimtirit58@gmail.com>
2024-01-10 16:51:00 +0300

    if logged in we se a welcome message and a logout option if we are not we see register and login

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -4,9 +4,20 @@
             <a href="/">Workopia</a>
         </h1>
         <nav class="space-x-4">
-            <a href="/auth/login" class="text-white hover:underline">Login</a>
-            <a href="/auth/register" class="text-white hover:underline">Register</a>
-            <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
+            <?php if (Framework\Session::has('user')) : ?>
+                <div class="flex justify-between items-center gap-4">
+                    <div class="text-blue-500">
+                        Welcome <?= Framework\Session::get('user')['name'] ?>
+                    </div>
+                    <form method="POST" action="/auth/logout">
+                        <button type="submit" class="text-white inline hover:underline">Logout</button>
+                    </form>
+                    <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
+                </div>
+            <?php else : ?>
+                <a href="/auth/login" class="text-white hover:underline">Login</a>
+                <a href="/auth/register" class="text-white hover:underline">Register</a>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
